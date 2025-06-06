8c2758768b8073bad9666333189946aec224eb85
Akshay Asok <akshayasokson@gmail.com>
2025-06-06 11:20:04 +0530

    counter app implemented

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,23 @@
-
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Counter</h1>
+    <div className="App" 
+    style={{
+      padding : "10px"
+    }}
+    >
+      <h1>Counter App</h1>
+      <p>{`Count: ${count}`}</p>
+      <button style={{ padding : '5px', margin : "3px"}} onClick={() => {
+        setCount(count+1);
+      }} >Increment</button>
+      <button style={{ padding : '5px', margin : "3px"}} onClick={() => {
+        setCount(count-1);
+      }} >Decrement</button>
     </div>
   );
 }
